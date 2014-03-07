afc89326ad913578b0ed65c811cd1946c7f96d8d
Gabriel Hao <gabriel.hao.wei@gmail.com>
2014-03-07 13:30:09 +0100

    add method to build cachemanager

--- clustered-mongodb-cache/src/main/java/org/haowei/clusteredcache/distribution/AbstractNode.java
+++ clustered-mongodb-cache/src/main/java/org/haowei/clusteredcache/distribution/AbstractNode.java
@@ -1,7 +1,13 @@
 package org.haowei.clusteredcache.distribution;
 
+import org.infinispan.manager.EmbeddedCacheManager;
+
 /**
  * Created by hao on 3/7/14.
  */
-public class AbstractNode {
+public abstract class AbstractNode {
+
+    private static EmbeddedCacheManager CreateCacheManagerProgramatically(){
+
+    }
 }
