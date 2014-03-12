e8ff7c10314e639748f4e99dd567e8cced14ebcf
gabrielhao <gabriel.hao.wei@gmail.com>
2014-03-12 19:19:53 +0100

    add node2

--- clustered-mongodb-cache/src/main/java/org/haowei/clusteredcache/distribution/Node2.java
+++ clustered-mongodb-cache/src/main/java/org/haowei/clusteredcache/distribution/Node2.java
@@ -1,7 +1,27 @@
 package org.haowei.clusteredcache.distribution;
 
+import org.haowei.clusteredcache.util.LoggingListener;
+import org.infinispan.Cache;
+
 /**
  * Created by hao on 3/12/14.
  */
-public class Node2 {
+public class Node2 extends AbstractNode {
+    @Override
+    protected int getNodeId(){
+        return 2;
+    }
+
+    public static void main(String[] args) throws Exception{
+        new Node2().run();
+    }
+
+    public void run(){
+        Cache<String, String> cache = getCacheManager().getCache("distCache");
+
+        cache.addListener(new LoggingListener());
+
+        waitForClusterToForm();
+    }
+
 }
